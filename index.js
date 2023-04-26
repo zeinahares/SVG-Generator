8fd0bed9c6e544b0a178ed87a2cbf1701514dc3f
zeinahares <zeinahares@gmail.com>
2023-04-27 02:33:03 +1000

    shapes classes aand first test written

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// ├── examples/           // Example svg file(s) created with the app
-// ├── lib/                // Folder for classes or functions
-//     ├── shapes.js       // Exports `Triangle`, `Circle`, and `Square` classes
-//     ├── shapes.test.js  // Jest tests for shapes
-//     └── more...         // Additional files and tests
-// ├── .gitignore          // Indicates which folders and files Git should ignore
-// ├── index.js            // Runs the application using imports from lib/
-// ├── package.json
-// └── README.md           // App description, link to video, setup and usage instructions       
-
 // GIVEN a command-line application that accepts user input
 // WHEN I am prompted for text
 // THEN I can enter up to three characters
